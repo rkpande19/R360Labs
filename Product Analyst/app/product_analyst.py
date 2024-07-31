2733cf6d0cdc70be58ddbe24c122ca2a877d2dd3
Rohit Kumar Pandey <rkpande@R36001M00274.local>
2024-07-31 10:24:26 +0530

    Added features to save the output in formatted pdf files

--- Product Analyst/app/product_analyst.py
+++ Product Analyst/app/product_analyst.py
@@ -6,7 +6,10 @@ from crewai_tools import tool
 import configparser
 import os
 from docx import Document
-
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_JUSTIFY
+import re
 class ProductAnalyst:
     """
     A class representing a product analyst.
@@ -172,6 +175,27 @@ def read_docx(file_path):
     doc = Document(file_path)
     return " ".join([paragraph.text for paragraph in doc.paragraphs])
 
+def markdown_to_html(text):
+    bold = re.compile(r'\*\*(.*?)\*\*')
+    
+    def repl(match):
+        return '<b>' + match.group(1) + '</b>'
+    return bold.sub(repl, text)
+
+def write_to_pdf(file_name, text):
+    directory = os.path.dirname(file_name)
+    if not os.path.exists(directory):
+        print("The directory doesn't exist. Please enter a valid directory.")
+        return False
+
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+    text = markdown_to_html(text)
+    doc = SimpleDocTemplate(file_name)
+    story = [Paragraph(line, styles["Justify"]) for line in text.split('\n') if line]
+    doc.build(story)
+    return True
+
 
 def main():
     """
@@ -183,14 +207,19 @@ def main():
     user_story_writer = create_agent(product_analyst, "write")
     ux_ui_requirements_generator = create_agent(product_analyst, "ux_ui")
     
+    is_analysed = False
+    is_user_stories_written = False
+    
     while True:
+        print ('\n I am bot crafted by a human to assist you in your product analysis journey. I can help you with the following tasks:')
         print("\n1. Analyse requirements")
         print("\n2. Write user stories")
         print("\n3. Generate UX/UI requirements")
         print("\n4. Exit")
         choice = input("\nPlease enter your choice: ")
         
-        if choice == "1":
+        
+        if choice == "1" or not is_analysed:
             input_text = input("Please enter the text to analyze or a file path ")
             if os.path.isfile(input_text):
                 file_extension = os.path.splitext(input_text)[1]
@@ -205,17 +234,33 @@ def main():
             task1 = create_tasks(product_analyst, "analyse", requirement_analyser)
             task1.description = product_analyst.config.get('TaskDescriptions', 'Analyse').format(input_text)
             crew = create_crew([requirement_analyser], [task1])
-            crew.kickoff()
-        elif choice == "2":
+            result = crew.kickoff()
+            while True:
+                file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
+                if write_to_pdf(file_name, result):
+                    break
+            is_analysed = True
+        elif choice == "2" or not is_user_stories_written:
             task2 = create_tasks(product_analyst, "write", user_story_writer)
             task2.description = product_analyst.config.get('TaskDescriptions', 'Write')
             crew = create_crew([user_story_writer], [task2])
-            crew.kickoff()
+            result = crew.kickoff()
+            file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
+            while True:
+                file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
+                if write_to_pdf(file_name, result):
+                    break
+            is_user_stories_written = True
         elif choice == "3":
             task3 = create_tasks(product_analyst, "ux_ui", ux_ui_requirements_generator)
             task3.description = product_analyst.config.get('TaskDescriptions', 'UXUI')
             crew = create_crew([ux_ui_requirements_generator], [task3])
-            crew.kickoff()
+            result = crew.kickoff()
+            while True:
+                file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
+                if write_to_pdf(file_name, result):
+                    break
+            write_to_pdf(file_name, result)
         elif choice == "4":
             break
         else:

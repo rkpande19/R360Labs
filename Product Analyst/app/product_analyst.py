7e5d5209b49645483495c75290530eb5622d22dc
Rohit Kumar Pandey <rkpande@R36001M00274.local>
2024-08-07 12:16:40 +0530

    added a feature to generate innovative design concepts

--- Product Analyst/app/product_analyst.py
+++ Product Analyst/app/product_analyst.py
@@ -87,6 +87,17 @@ class ProductAnalyst:
         self.allow_delegation = self.config['UXUIRequirementsGenerator']['allow_delegation']
         self.llm = Ollama(model=self.config['ProductAnalyst']['llm'])
         
+    def innovation_ux_experience_a(self):
+        """
+        Get the information from the research on the concept note and the requirement analysis to provide detailed suggestion for user experience and innovation.
+        """
+        self.role = self.config['InnovationAndUXExperience']['role']
+        self.goal = self.config['InnovationAndUXExperience']['goal']
+        self.backstory = self.config['InnovationAndUXExperience']['backstory']
+        self.verbose = self.config['InnovationAndUXExperience']['verbose']
+        self.allow_delegation = self.config['InnovationAndUXExperience']['allow_delegation']
+        self.llm = Ollama(model=self.config['ProductAnalyst']['llm'])
+        
     def concept_research_t(self):
         """
         Sets the task description and output for concept research.
@@ -115,6 +126,13 @@ class ProductAnalyst:
         """
         self.description = self.config['UXUIRequirementsGenerator']['task_description']
         self.output = self.config['UXUIRequirementsGenerator']['task_output']
+        
+    def innovation_ux_experience_t(self):
+        """
+        Sets the task description and output for innovation and UX experience.
+        """
+        self.description = self.config['InnovationAndUXExperience']['task_description']
+        self.output = self.config['InnovationAndUXExperience']['task_output']
 
 
 def create_agent(product_analyst, type):
@@ -132,6 +150,8 @@ def create_agent(product_analyst, type):
         product_analyst.concept_research_a()
     elif type.lower() == "analyse":
         product_analyst.requirement_analysis_a()
+    elif type.lower() == "innovation":
+        product_analyst.innovation_ux_experience_a()
     elif type.lower() == "write":
         product_analyst.user_stories_a()
     elif type.lower() == "ux_ui":
@@ -167,6 +187,8 @@ def create_tasks(product_analyst, type, agent):
         product_analyst.concept_research_t()
     elif type.lower() == "analyse":
         product_analyst.requirement_analysis_t()
+    elif type.lower() == "innovation":
+        product_analyst.innovation_ux_experience_t()
     elif type.lower() == "write":
         product_analyst.user_stories_t()
     elif type.lower() == "ux_ui":
@@ -232,23 +254,24 @@ def main():
     product_analyst.load_config()
     concept_researcher = create_agent(product_analyst, "research")
     requirement_analyser = create_agent(product_analyst, "analyse")
+    innovator = create_agent(product_analyst, "innovation")
     user_story_writer = create_agent(product_analyst, "write")
     ux_ui_requirements_generator = create_agent(product_analyst, "ux_ui")
     
-    is_analysed = False
-    is_user_stories_written = False
+
     
     while True:
         print ('\n I am a bot crafted by a human to assist you in your product analysis journey. I can help you with the following tasks:')
         print("\n1. Research on concept note")
         print("\n2. Analyse requirements")
-        print("\n3. Write user stories")
-        print("\n4. Generate UX/UI requirements")
-        print("\n5. Exit")
+        print("\n3. Suggest innovative ideas for the concept note")
+        print("\n4. Write user stories")
+        print("\n5. Generate UX/UI requirements")
+        print("\n6. Exit")
         choice = input("\nPlease enter your choice: ")
         choice = str(choice)
         
-        if choice == "1":
+        if (choice == "1") and choice != "6":
             input_text = input("Please enter the text to analyze or a file path ")
             if os.path.isfile(input_text):
                 file_extension = os.path.splitext(input_text)[1]
@@ -260,15 +283,17 @@ def main():
                 else:
                     print("Invalid file format. Please enter a text file")
                     continue
-            task1 = create_tasks(product_analyst, "research", concept_researcher)
-            task1.description = product_analyst.config.get('TaskDescriptions', 'Research').format(input_text)
-            crew = create_crew([concept_researcher], [task1])
+            task = create_tasks(product_analyst, "research", concept_researcher)
+            task.description = product_analyst.config.get('TaskDescriptions', 'Research').format(input_text)
+            crew = create_crew([concept_researcher], [task])
             result = crew.kickoff()
             while True:
                 file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
                 if write_to_pdf(file_name, result):
-                    break 
-        elif (choice == "2" or not is_analysed) and choice != "5":
+                    print("The output has been saved to the file.")
+                    break
+            
+        elif (choice == "2") and choice != "6":
             input_text = input("Please enter the text to analyze or a file path ")
             if os.path.isfile(input_text):
                 file_extension = os.path.splitext(input_text)[1]
@@ -280,19 +305,52 @@ def main():
                 else:
                     print("Invalid file format. Please enter a text file")
                     continue
-            task1 = create_tasks(product_analyst, "analyse", requirement_analyser)
-            task1.description = product_analyst.config.get('TaskDescriptions', 'Analyse').format(input_text)
-            crew = create_crew([requirement_analyser], [task1])
+            task_research = create_tasks(product_analyst, "research", concept_researcher)
+            task_research.description = product_analyst.config.get('TaskDescriptions', 'Research').format(input_text)      
+            task_analyse = create_tasks(product_analyst, "analyse", requirement_analyser)
+            task_analyse.description = product_analyst.config.get('TaskDescriptions', 'Analyse').format(input_text)
+            crew = create_crew([concept_researcher,requirement_analyser], [task_research, task_analyse])   
             result = crew.kickoff()
             while True:
                 file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
-                if write_to_pdf(file_name, result):
+                if write_to_pdf(file_name, result) and choice == "2":
+                    print("The output has been saved to the file.")
                     break
             is_analysed = True
-        elif (choice == "3" or not is_analysed) and choice != "5":
-            task2 = create_tasks(product_analyst, "write", user_story_writer)
-            task2.description = product_analyst.config.get('TaskDescriptions', 'Write')
-            crew = create_crew([user_story_writer], [task2])
+        elif choice == "3" and choice != "6":
+            input_text = input("Please enter the text to analyze or a file path ")
+            if os.path.isfile(input_text):
+                file_extension = os.path.splitext(input_text)[1]
+                if file_extension == ".txt":
+                    with open(input_text, "r") as file:
+                        input_text = file.read()
+                elif file_extension in ['.doc', '.docx']:
+                    input_text = read_docx(input_text)
+                else:
+                    print("Invalid file format. Please enter a text file")
+                    continue
+            task_innovate = create_tasks(product_analyst, "innovation", innovator)
+            task_innovate.description = product_analyst.config.get('TaskDescriptions', 'InnovationAndUXExperience')
+            task_research = create_tasks(product_analyst, "research", concept_researcher)
+            task_research.description = product_analyst.config.get('TaskDescriptions', 'Research').format(input_text)    
+            task_analyse = create_tasks(product_analyst, "analyse", requirement_analyser)
+            task_analyse.description = product_analyst.config.get('TaskDescriptions', 'Analyse').format(input_text)
+            crew = create_crew([concept_researcher,requirement_analyser,innovator], [task_research, task_analyse, task_innovate])
+            result = crew.kickoff()
+            while True:
+                file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
+                if write_to_pdf(file_name, result):
+                    print("The output has been saved to the file.")
+                    break
+            
+        elif choice == "4" and choice != "6":
+            task_write_user_stories = create_tasks(product_analyst, "write", user_story_writer)
+            task_write_user_stories.description = product_analyst.config.get('TaskDescriptions', 'Write')
+            task_research = create_tasks(product_analyst, "research", concept_researcher)
+            task_research.description = product_analyst.config.get('TaskDescriptions', 'Research').format(input_text)      
+            task_analyse = create_tasks(product_analyst, "analyse", requirement_analyser)
+            task_analyse.description = product_analyst.config.get('TaskDescriptions', 'Analyse').format(input_text)
+            crew = create_crew([user_story_writer], [task_research, task_analyse, task_write_user_stories])
             result = crew.kickoff()
             file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
             while True:
@@ -300,17 +358,18 @@ def main():
                 if write_to_pdf(file_name, result):
                     break
             is_user_stories_written = True
-        elif (choice == "4" or not is_user_stories_written) and choice != "5":
-            task3 = create_tasks(product_analyst, "ux_ui", ux_ui_requirements_generator)
-            task3.description = product_analyst.config.get('TaskDescriptions', 'UXUI')
-            crew = create_crew([ux_ui_requirements_generator], [task3])
+        elif choice == "5" and choice != "6":
+            task = create_tasks(product_analyst, "ux_ui", ux_ui_requirements_generator)
+            task.description = product_analyst.config.get('TaskDescriptions', 'UXUI')
+            crew = create_crew([ux_ui_requirements_generator], [task])
             result = crew.kickoff()
             while True:
                 file_name = input('Please enter the file name and location to save the output in the format <location>/<filename>.pdf: ')
                 if write_to_pdf(file_name, result):
+                    print("The output has been saved to the file.")
                     break
             write_to_pdf(file_name, result)
-        elif choice == "5":
+        elif choice == "6":
             print("Thank you for using the product analyst bot. Goodbye!")
             break
         else:
